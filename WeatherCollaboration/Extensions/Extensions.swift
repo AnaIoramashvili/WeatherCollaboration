3e3a915f800a93e4fd0d7b3a20ea100551f3799d
RaisaBadal5 <raya.badalova@gmail.com>
2024-06-14 22:50:44 +0400

    commit extensions

--- WeatherCollaboration/Extensions/Extensions.swift
+++ WeatherCollaboration/Extensions/Extensions.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+extension Int {
+    func formatHour() -> String {
+        return String(format: "%02d", self)
+    }
+}
+
+extension Int {
+    func dayOfWeek() -> String {
+        let date = Date(timeIntervalSince1970: TimeInterval(self))
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        return dateFormatter.string(from: date)
+    }
+}
+
+extension Int {
+    func formatUnixTimestamp() -> String {
+        let date = Date(timeIntervalSince1970: TimeInterval(self))
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        return formatter.string(from: date)
+    }
+}
+
