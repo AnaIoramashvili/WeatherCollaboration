30ff8ca2e6f28df871c2048cdcf93509b4a19e5c
Vako <valeri.mekhashishvili97@gmail.com>
2024-06-14 22:35:15 +0400

    added MainViews

--- WeatherCollaboration/Views/MainViews/ContentView.swift
+++ WeatherCollaboration/Views/MainViews/ContentView.swift
@@ -8,16 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject var cityStore: CityStore
+    @StateObject private var weatherVM = WeatherViewModel()
+    @State private var isSearching = false
+    @State private var searchText = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            ScrollView {
+                VStack {
+                    if let weather = weatherVM.weatherInfo {
+                        WeatherDetailView(weather: weather)
+                    } else {
+                        Text("Select a city to see weather details")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                            .padding()
+                    }
+                    
+                    Spacer()
+                    
+                    NavigationLink(
+                        destination: SearchPageView(isPresented: $isSearching, viewModel: weatherVM),
+                        isActive: $isSearching
+                    ) {
+                    }
+                }
+            }
+           
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Menu {
+                        ForEach(cityStore.cities, id: \.self) { city in
+                            Button(action: {
+                                weatherVM.selectedCity = city
+                                weatherVM.fetchCityDetails(for: city.name)
+                            }) {
+                                Text(city.name)
+                            }
+                        }
+                        Button("Add New Location") {
+                            isSearching = true
+                        }
+                    } label: {
+                        Text(weatherVM.selectedCity?.name ?? "Weather App")
+                            .font(.largeTitle)
+                            .foregroundColor(.black)
+                            .padding()
+                    }
+                }
+            }
+            .onAppear {
+                
+                cityStore.loadCities()
+                if let firstCity = cityStore.cities.first  {
+                    if !weatherVM.didFetchFirst {
+                        weatherVM.selectedCity = firstCity
+                        weatherVM.fetchCityDetails(for: firstCity.name)
+                        print(firstCity.name)
+                        weatherVM.didFetchFirst = true
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
+
+
+
 
 #Preview {
     ContentView()
