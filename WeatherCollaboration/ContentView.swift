b3c44b5ba65e7c3405c81ef7a4496a9044902d81
Ioramka <ana.ioramashvili.1@btu.edu.ge>
2024-06-14 21:51:51 +0400

    Initial Commit

---
+++ WeatherCollaboration/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherCollaboration
+//
+//  Created by Ana on 6/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherCollaboration/WeatherCollaborationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCollaborationApp.swift
+//  WeatherCollaboration
+//
+//  Created by Ana on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherCollaborationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
